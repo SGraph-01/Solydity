04e1950b4ef619271ce84796211d6c6743d6152f
SGrpah <78150284+SGraph-01@users.noreply.github.com>
2023-10-16 15:39:36 +0500

    Update math.sol

--- math.sol
+++ math.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract math {
-    //execution cost 963 gas
+    //execution cost 1052 gas
     function add(uint a, uint b) public pure returns (uint) {
         return a + b;
     }
 
-    //execution cost 759 gas
+    //execution cost 826 gas
     //no overflow check, does not throw an exception on overflow
     function add2(uint a, uint b) public pure returns (uint result) {
         assembly {
             result := add(a, b)
+        }
+    }
+
+    //execution cost 808 gas
+    // Check for overflow
+    function add3(uint x, uint y) public pure returns (uint z) {
+        assembly {
+            z := add(x, y)
+            if lt(z, x) { revert(0, 0) }
         }
     }
 
@@ -20,11 +29,20 @@ contract math {
         return a - b;
     }
 
-    //execution cost 781 gas
+    //execution cost 891 gas
     //no overflow check, does not throw an exception on overflow
-    function subA(uint a, uint b) public pure returns (uint result) {
+    function sub2(uint a, uint b) public pure returns (uint result) {
         assembly {
             result := sub(a, b)
+        }
+    }
+
+    //execution cost 873 gas
+    // Check for overflow
+    function sub3(uint x, uint y) public pure returns (uint z) {
+        assembly {
+            if lt(x, y) { revert(0, 0) }
+            z := sub(x, y)
         }
     }
 
@@ -33,11 +51,26 @@ contract math {
         return a * b;
     }
 
-    //execution cost 783 gas
+    //execution cost 850 gas
     //no overflow check, does not throw an exception on overflow
-    function mulA(uint a, uint b) public pure returns (uint result) {
+    function mul2(uint a, uint b) public pure returns (uint result) {
         assembly {
             result := mul(a, b)
+        }
+    }
+
+    //execution cost 967 gas
+    // Check for overflow
+    function mul3(uint x, uint y) public pure returns (uint z) {
+        assembly {
+            switch iszero(x)
+            case 1 {
+                z := 0
+            }
+            default {
+                z := mul(x, y)
+                if iszero(eq(div(z, x), y)) { revert(0, 0) }
+            }
         }
     }
 
@@ -47,12 +80,21 @@ contract math {
         return a / b;
     }
 
-    //execution cost 806 gas
+    //execution cost 829 gas
     //no overflow check, does not throw an exception on overflow
-    function divA(uint256 a, uint256 b) public pure returns (uint256 result) {
+    function div2(uint256 a, uint256 b) public pure returns (uint256 result) {
         require(b != 0, "denominator == 0");
         assembly {
             result := div(a, b)
+        }
+    }
+
+    //execution cost 800 gas
+    // Check for overflow
+    function div3(uint x, uint y) public pure returns (uint z) {
+        assembly {
+            if iszero(y) { revert(0, 0) } 
+            z := div(x, y)                
         }
     }
 }
